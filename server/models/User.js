c850a293cbeef7966bc16b456f9d330e70f80f9d
sam0127-jan <mohiteaniket248@gmail.com>
2026-05-23 23:18:34 +0530

    add branch year and college in user model

--- server/models/User.js
+++ server/models/User.js
@@ -1,13 +1,24 @@
 const mongoose = require("mongoose")
 
 const userSchema = new mongoose.Schema({
+
     name: String,
+
     email: String,
+
     password: String,
+
+    branch: String,
+
+    year: String,
+
+    college: String,
+
     role: {
         type: String,
         default: "student"
     }
+
 })
 
 module.exports = mongoose.model("User", userSchema)
