6458d77ba84f473f0f1d8a723d4696ef4b6a2535
sam0127-jan <mohiteaniket248@gmail.com>
2026-05-23 19:49:32 +0530

    fix server routes and test api

--- server/server.js
+++ server/server.js
@@ -1,45 +1,60 @@
 const express = require("express")
 const mongoose = require("mongoose")
 const cors = require("cors")
+
 require("dotenv").config()
 
 const app = express()
 
-// Middlewares
+// MIDDLEWARES
 app.use(cors())
+
 app.use(express.json())
 
-// MongoDB connect
+// CONNECT MONGODB
 mongoose
   .connect(process.env.MONGO_URL)
-  .then(() => console.log("MongoDB Connected"))
-  .catch((err) => console.log(err))
+  .then(() => {
+    console.log("MongoDB Connected")
+  })
+  .catch((err) => {
+    console.log(err)
+  })
 
-// Test route
+// HOME ROUTE
 app.get("/", (req, res) => {
   res.send("Server is running")
 })
 
-// Import routes
+// IMPORT ROUTES
 const authRoutes = require("./routes/auth")
+
 const adminRoutes = require("./routes/admin")
+
 const testRoutes = require("./routes/test")
+
 const resultRoutes = require("./routes/result")
 
-// Use routes
+const announcementRoutes = require("./routes/announcement")
+
+const activityRoutes = require("./routes/activity")
+
+// USE ROUTES
 app.use("/api/auth", authRoutes)
+
 app.use("/api/admin", adminRoutes)
+
 app.use("/api/tests", testRoutes)
+
 app.use("/api/results", resultRoutes)
 
+app.use("/api/announcements", announcementRoutes)
+
+app.use("/api/activities", activityRoutes)
+
+// PORT
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => {
-  console.log("Server running on port", PORT)
+  console.log(`Server running on port ${PORT}`)
 })
-
-const announcementRoutes = require("./routes/announcement")
-const activityRoutes = require("./routes/activity")
-
-app.use("/api/announcements", announcementRoutes)
-app.use("/api/activities", activityRoutes)
